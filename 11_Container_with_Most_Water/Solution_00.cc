b7ecd1b998a7d5401895be18fb55e00ea68e11aa
wuqingze <nongxiaolang@foxmail.com>
2020-10-25 06:31:49 +0800

    11 Container with Most Water

--- 11_Container_with_Most_Water/Solution_00.cc
+++ 11_Container_with_Most_Water/Solution_00.cc
@@ -7,14 +7,12 @@ using namespace std;
 class Solution{
     public:
         static int maxArea(vector<int> &height){
-            int i=0, j=height.size()-1;
             int mx = 0;
-            while(i<j){
-                int mn = (height[i]<height[j]?height[i]:height[j])*(j-i);
-                mx = mx>mn?mx:mn;
-                if(height[i]<height[j]) i+=1;
-                else j-=1;
-            }
+            for(int i=0;i<height.size()-1;i++)
+                for(int j=i+1;j<height.size();j++){
+                    int container = (height[i]<height[j]?height[i]:height[j])*(j-i);
+                    mx = mx>container?mx:container;
+                }
             return mx;
         }
 };
