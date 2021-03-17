d58fc7d9196bdd63db19655e911d3687f9085e44
wuqingze <nongxiaolang@foxmail.com>
2021-03-17 15:02:13 +0800

    0068 Text Justification

--- 0068-Text-Justification/Solution.cc
+++ 0068-Text-Justification/Solution.cc
@@ -12,65 +12,47 @@ public:
     }
 
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        vector<string> ret;
         int n = words.size();
         int i=0, j=0;
+        vector<string> ret;
         while(true){
-            int c=0;
+            int c = 0;
             while(j<n){
                 c += words[j].size();
-                if(c+j-i>maxWidth) break;
+                if(c+(j-i)>maxWidth) break;
                 j += 1;
             }
             if(j<n){
                 c -= words[j].size();
                 j -= 1;
-                int balance = maxWidth -c;
-                int mn = balance/(j-i);
-                int mod = balance%(j-i);
-                string mxspace = space(mn+1);
-                string mnspace = space(mn);
                 string t = "";
-                for(int k=i;k<=j-1;k++){
-                    if(mod-->0){
-                        t += words[k]+mxspace;
-                    }else{
-                        t += words[k]+ mnspace;
+                if(j ==i){
+                    t += words[i]+space(maxWidth-words[i].size());
+                }else{
+                    int mn = (maxWidth-c)/(j-i);
+                    int mod = (maxWidth-c)%(j-i);
+                    string mnstr = space(mn);
+                    for(int k=i;k<j;k++){
+                        if(mod-->0){
+                            t += words[k]+mnstr+" ";
+                        }else{
+                            t += words[k]+mnstr;
+                        }
                     }
+                    t += words[j];
                 }
-                t += words[j];
                 ret.push_back(t);
                 i = j+1;
                 j += 1;
             }else{
-                j -= 1;
-                if(c+j-i<=maxWidth){
-                    string t = "";
-                    for(int k=i;k<j;k++){
-                        t += words[k] + " ";
-                    }
-                    t += words[j];
-                    ret.push_back(t);
-                }else{
-                    c -= words[j].size();
-                    j -= 1;
-                    int balance = maxWidth - c;
-                    int mn = balance/(j-i);
-                    int mod = balance%(j-i);
-                    string mxspace = string(" ", mn+1);
-                    string mnspace = string(" ", mn);
-                    string t =  "";
-                    for(int k=i;k<=j-1;k++){
-                        if(mod-->0){
-                            t += words[k]+mxspace;
-                        }else{
-                            t += words[k]+mnspace;
-                        }
-                    }
-                    t += words[j];
-                    ret.push_back(t);
-                    ret.push_back(words[n-1]);
+                string t = "";
+                for(int k=i;k<j-1;k++){
+                    t += words[k] + " ";
                 }
+                t += words[j-1];
+                int gap = maxWidth - (c+(j-i-1));
+                t += space(gap);
+                if(t != "") ret.push_back(t);
                 break;
             }
         }
