32f2672a0daab4f2f644d718c225871282a2445d
wuqingze <nongxiaolang@foxmail.com>
2021-03-29 18:19:15 +0800

    0147-Insertion-Sort-List

---
+++ 0147-Insertion-Sort-List/Solution.cc
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        auto ptr = head;
+        vector<ListNode*> l;
+        while(ptr){
+            l.push_back(ptr);
+            ptr = ptr->next;
+        }
+        sort(l.begin(), l.end(), [](ListNode* o1, ListNode* o2){
+            return o1->val < o2->val;
+        });
+        int n = l.size();
+        for(int i=0;i<n-1;i++){
+            l[i]->next = l[i+1];
+        }
+        l[n-1]->next = nullptr;
+        return l[0];
+    }
+};
